0308376b082293bb34193a94a0f0f5a43dfa444e
Francesca <fracassani@gmail.com>
2021-01-11 17:00:29 +0100

    Homepage

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,1 +1,34 @@
-<footer>FOOTER</footer>
+<footer>
+    <section class="content-info">
+        <div class="wrapper pb-2">
+            <a href="#" class="no-list">
+                <ul class="flex jc-center no-list">
+                    <li class="pr-4">
+                        <h2>Movies</h2>
+                        <p>Wonder Woman</p>
+                        <p>Black Widow</p>
+                        <p>Ghostbusters</p>
+                        <p>No Time To Die</p>
+                    </li>
+    
+                    <li class="pr-4">
+                        <h2>Tv</h2>
+                        <p>TV Recaps</p>
+                        <p>TV This Week</p>
+                        <p>The Mandalorian</p>
+                        <p>WandaVision</p>
+                    </li>
+    
+                    <li class="pr-4">
+                        <h2>Comics</h2>
+                        <p>Marvel</p>
+                        <p>DC</p>
+                        <p>Image</p>
+                        <p>IDW</p>
+                    </li>
+
+                </ul>
+            </a>
+        </div>
+    </section>
+</footer>
